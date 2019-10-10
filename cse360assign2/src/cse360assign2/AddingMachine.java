cba4ad4b120d0501609ccdb1ae505c26a0774fb0
evanw <evanw@DESKTOP-HMDESKJ>
2019-10-09 20:52:27 -0700

    CSE360 Assignment 2

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,71 @@
+/**        
+ * Name: Evan Wendt
+ * Assignment #: 2
+ * Class ID: 70642 (CSE 360 W (9:40-10:30AM) Seating ID: R E 1
+ * Description: This simple calculator class is being used to obtain practice in using a version control system (github).
+  */
+
+package cse360assign2;
+
+/**
+ * This class can add or subtract from a total variable that is initially set at zero.
+ * @author Evan Wendt
+ * @version 1.0
+ *
+ */
+
+public class AddingMachine {
+
+	private int total;
+	
+	/**
+	 * A constructor that sets the initial total to zero
+	 */
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Returns the current total
+	 * @return current total
+	 */
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * adds the parameter to the total
+	 * @param value is the integer added to the total
+	 */
+	
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * subtracts the parameter from the total
+	 * @param value is the integer subtracted from the total
+	 */
+	
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Returns the history of operations done to the total
+	 */
+		
+	public String toString () {
+		return "";
+	}
+	
+	/**
+	 * Sets the total to zero
+	 */
+	public void clear() {
+	
+	}
+}
+
